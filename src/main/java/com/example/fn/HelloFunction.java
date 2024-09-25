a8176e63201a7e5c037ee665dc76255ff82aa255
ShahvaizJanjua <shahvaiz.janjua@gmail.com>
2024-09-25 22:37:49 +0000

    downloadfromOS

--- src/main/java/com/example/fn/HelloFunction.java
+++ src/main/java/com/example/fn/HelloFunction.java
@@ -3,10 +3,26 @@ package com.example.fn;
 public class HelloFunction {
 
     public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
+        ObjectMapper objectMapper = new ObjectMapper();
+        //Map data = objectMapper.convertValue(event.getData().get(), Map.class);
+        //Map additionalDetails = objectMapper.convertValue(data.get("additionalDetails"), Map.class);
 
-        System.out.println("Inside Java Hello World function"); 
-        return "Hi, " + name + "!";
+        String objectURL = "https://frofufwuvghu.objectstorage.uk-london-1.oci.customer-oci.com/p/w7dj07NLfA0cJcKaIGl7oJwLiZaZmjkOzjaFDLeDfQKzauu9muot5HqVj_NXLYd0/n/frofufwuvghu/b/SJBucket/o/"
+        +input;
+
+        /*try (BufferedInputStream in = new BufferedInputStream(new URL(objectURL).openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(input)) {
+                byte dataBuffer[] = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                    fileOutputStream.write(dataBuffer, 0, bytesRead);
+                }   
+            } catch (IOException e) {
+             // handle exception
+        }*/
+        InputStream in = new URL(objectURL).openStream();
+        Files.copy(in, Paths.get(input), StandardCopyOption.REPLACE_EXISTING);
+
     }
 
 }
